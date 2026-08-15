21d11aa6f87f3367a3165a9328f924a3c375da69
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-15 14:22:39 +0530

    refactor: improve validation error messages and code formatting in checkValidData function

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,10 +1,13 @@
 export const checkValidData = (email, password) => {
-    const isEmailValid =
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
-    const isPasswordValid = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/.test(password);
+  const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+    email,
+  );
+  const isPasswordValid =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/.test(password);
+  const errMessage = "Invalid email or password.";
 
-    if (!isEmailValid) return "Email is not valid";
-    if (!isPasswordValid) return "Password is not valid";
+  if (!isEmailValid) return errMessage;
+  if (!isPasswordValid) return errMessage;
 
-    return "Validate Successfully";
-}
+  return null;
+};
