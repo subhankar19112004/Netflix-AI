5748107aad3ae01ff8e821c8a956899cfc62a492
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-15 14:23:06 +0530

    feat: implement Firebase authentication for sign-in and sign-up with error handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,15 @@ import { useRef, useState } from "react";
 import { BACKGROUND_IMAGE_URL } from "../utils/constants";
 import Header from "./Header.jsx";
 import { checkValidData } from "../utils/validate.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../firebase/firebase.js";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const email = useRef(null);
   const password = useRef(null);
@@ -15,10 +21,53 @@ const Login = () => {
 
   const handleButtonClick = (e) => {
     e.preventDefault();
-    const validationMessage = checkValidData(email.current.value, password.current.value);
-    console.log(validationMessage);
-    console.log("email: " + email.current.value);
-    console.log("password: " + password.current.value);
+    if (!email.current.value || !password.current.value) {
+      setErrorMessage("Email and password fields cannot be empty.");
+      return;
+    }
+    const validationMessage = checkValidData(
+      email.current.value,
+      password.current.value,
+    );
+    setErrorMessage(validationMessage);
+    if (validationMessage) return;
+    // Sign-Up Logic
+
+    if (!isSignIn) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value,
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorMessage + " (Error Code: " + errorCode + ")");
+        });
+    } else {
+      // Sign-In Logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value,
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorMessage + " (Error Code: " + errorCode + ")");
+        });
+    }
   };
   return (
     <div className="relative min-h-auto">
@@ -33,7 +82,11 @@ const Login = () => {
         <div className="absolute inset-0 bg-black/60"></div>
       </div>
 
-      <form className="relative flex flex-col items-start justify-center bg-black/80 w-[90%] max-w-md min-h-100 m-auto mt-50 p-10 rounded-md text-white">
+      <form
+        className={`relative flex flex-col items-start justify-center bg-black/80 w-[90%] max-w-md min-h-100 m-auto mt-50 p-10 rounded-md text-white
+        
+        ${errorMessage ? "border-2 border-black  shadow-2xl shadow-red-600 " : "border-2 border-black  shadow-2xl shadow-green-200"}`}
+      >
         <p className="text-2xl font-light font-serif mb-6">
           {isSignIn ? "Sign In" : "Sign Up"}
         </p>
@@ -57,10 +110,13 @@ const Login = () => {
           placeholder="Enter the password"
           className="w-full bg-[#333] text-white p-4 rounded-md outline-none placeholder-gray-400 font-extralight font-serif"
         />
+        <p className="text-red-500 font-thin font-mono text-sm mt-2">
+          {errorMessage}
+        </p>
 
         <button
           onClick={handleButtonClick}
-          className="w-full bg-[#e50914] text-white p-3 rounded-md mt-6 font-mono font-light hover:bg-[#f40612] cursor-pointer "
+          className="w-full bg-[#e50914] text-white p-3 rounded-md mt-4 font-mono font-light hover:bg-[#f40612] cursor-pointer "
         >
           {isSignIn ? "Sign In" : "Sign Up"}
         </button>
