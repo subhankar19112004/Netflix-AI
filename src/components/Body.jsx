88507b37a4d2687190797522a7405daa74a95bdf
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-20 10:21:00 +0530

    refactor: remove unused authentication logic from Body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,9 @@
-import { useDispatch } from "react-redux";
 import Browse from "./Browse";
 import Login from "./Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useEffect } from "react";
-import { addUser, removeUser } from "../utils/redux/userSlice";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase/firebase";
 import Error from "./Error";
 
 const Body = () => {
-  const dispatch = useDispatch();
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -25,16 +19,7 @@ const Body = () => {
     }
   ]);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const {uid, email, displayName, photoURL} = user;
-        dispatch(addUser({ uid, email, displayName, photoURL }));
-      } else {
-        dispatch(removeUser());
-      }
-    });
-  }, []);
+
   return (
     <>
       <RouterProvider router={appRouter} />
