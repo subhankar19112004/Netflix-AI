65f910c60ddd383f124acd4712feb197820aeca8
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-21 21:19:23 +0530

    Added API call for now Playing Movies

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,11 +1,28 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
 
 const Browse = () => {
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      API_OPTIONS,
+    );
+
+    const result = await data.json();
+    console.log(result);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
+  
   return (
     <div>
-      <Header/>
+      <Header />
     </div>
-  )
-}
+  );
+};
 
 export default Browse;
