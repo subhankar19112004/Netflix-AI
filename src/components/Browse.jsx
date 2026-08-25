76df8910bd56532d5f4585e89aead9a766a5e19b
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-25 10:57:41 +0530

    feat: replace API call with custom hook for fetching now playing movies

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,23 +1,8 @@
-import { useEffect } from "react";
-import { API_OPTIONS, API_URL } from "../utils/constants";
+import { useNowPlayingMovies } from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      API_URL,
-      API_OPTIONS,
-    );
-
-    const result = await data.json();
-    console.log(result);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
-
-  
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
