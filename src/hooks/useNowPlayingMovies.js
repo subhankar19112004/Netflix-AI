de07f2a497f24b3efa3c6030c011fe6d02ccbd0f
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-26 11:49:17 +0530

    refactor: remove console log from useNowPlayingMovies hook

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -9,7 +9,6 @@ export const useNowPlayingMovies = () => {
         const data = await fetch(API_URL, API_OPTIONS);
 
         const json = await data.json();
-        console.log(json.results);
         dispatch(addNowPlayingMovies(json.results));
       };
 
