a82ab0704a423ac5c22fdcf0dd79d39d2d3b6b2b
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-27 13:46:57 +0530

    add custom hook useMovieTrailer to fetch and manage movie trailer data

---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrailerVideo } from "../utils/redux/movieSlice";
+import { useEffect } from "react";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  const getMovieVideo = async (movieId) => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos`,
+      API_OPTIONS,
+    );
+    const json = await data.json();
+
+    const filetrTrailersData = json.results.filter(
+      (video) => video.type === "Trailer",
+    );
+    let trailer = filetrTrailersData.length
+      ? filetrTrailersData[1]
+      : json.results[0];
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    getMovieVideo(movieId);
+  }, [movieId]);
+
+  return trailerVideo;
+};
+
+export default useMovieTrailer;
