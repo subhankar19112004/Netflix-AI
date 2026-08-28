a7ad2eb18886efe72171e3f1343e27fb13fae676
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-28 12:24:25 +0530

    add usePopularMovies hook to Browse component for fetching popular movies

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,10 +1,13 @@
 import { useNowPlayingMovies } from "../hooks/useNowPlayingMovies";
+import { usePopularMovies } from "../hooks/usePopularMovies";
 import MainContainer from "./browse_containers/MainContainer";
 import SecondaryContainer from "./browse_containers/SecondaryContainer";
 import Header from "./Header";
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
+  
   return (
     <>
       <Header />
