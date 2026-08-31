d61abffc4a5c31d14b472bfbf28905d1f6d00cca
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-31 14:55:42 +0530

    feat: integrate GPT search functionality into Browse component

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,18 +1,27 @@
+import { useSelector } from "react-redux";
 import { useNowPlayingMovies } from "../hooks/useNowPlayingMovies";
 import { usePopularMovies } from "../hooks/usePopularMovies";
 import MainContainer from "./browse_containers/MainContainer";
 import SecondaryContainer from "./browse_containers/SecondaryContainer";
+import GptSearch from "./GptSearch";
 import Header from "./Header";
 
 const Browse = () => {
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   useNowPlayingMovies();
   usePopularMovies();
-  
+
   return (
     <>
       <Header />
-      <MainContainer />
-      <SecondaryContainer/>
+      {showGptSearch ? (
+        <GptSearch />
+      ) : (
+        <>
+          <MainContainer />
+          <SecondaryContainer />
+        </>
+      )}
     </>
   );
 };
