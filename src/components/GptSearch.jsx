23b40ebe861aa954ed1dcf0121e68545bd2835ff
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-31 14:56:05 +0530

    feat: add GptSearch component to integrate GPT search bar

---
+++ src/components/GptSearch.jsx
@@ -0,0 +1,11 @@
+import GptSearchBar from "./GPT/GptSearchBar"
+
+const GptSearch = () => {
+  return (
+    <div className="bg-[#162e31] h-screen">
+      <GptSearchBar />
+    </div>
+  )
+}
+
+export default GptSearch
