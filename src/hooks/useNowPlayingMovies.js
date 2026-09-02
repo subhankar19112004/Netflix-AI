42ded28e5600f62e0452a1b4f80aeaba34c4fc5e
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-09-02 11:41:22 +0530

    feat: optimize now playing movies fetching logic with conditional dispatch

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,18 +1,21 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS, API_URL } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/redux/movieSlice";
 import { useEffect } from "react";
 
 export const useNowPlayingMovies = () => {
-      const dispatch = useDispatch();
-      const getNowPlayingMovies = async () => {
-        const data = await fetch(API_URL, API_OPTIONS);
+  const nowPlayingMovies = useSelector(
+    (state) => state.movies.nowPlayingMovies,
+  );
+  const dispatch = useDispatch();
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(API_URL, API_OPTIONS);
 
-        const json = await data.json();
-        dispatch(addNowPlayingMovies(json.results));
-      };
+    const json = await data.json();
+    dispatch(addNowPlayingMovies(json.results));
+  };
 
-      useEffect(() => {
-        getNowPlayingMovies();
-      }, []);
-}
+  useEffect(() => {
+    !nowPlayingMovies && getNowPlayingMovies();
+  }, []);
+};
