7a8c7cdd37340d01da9c14b928d36678d600e297
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-09-02 10:16:00 +0530

    feat: add loading state and movie results handling to gptSlice

--- src/utils/redux/gptSlice.js
+++ src/utils/redux/gptSlice.js
@@ -2,15 +2,37 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const gptSlice = createSlice({
     name: "gpt",
+
     initialState: {
-        showGptSearch: false
+        showGptSearch: false,
+        movieResults: null,
+        movieNames: null,
+        isLoading: false,
     },
+
     reducers: {
         toggleGptSearch: (state) => {
             state.showGptSearch = !state.showGptSearch;
-        }
-    }
+        },
+
+        setGptLoading: (state, action) => {
+            state.isLoading = action.payload;
+        },
+
+        addGptMovieResults: (state, action) => {
+            const { movieNames, movieResults } = action.payload;
+
+            state.movieNames = movieNames;
+            state.movieResults = movieResults;
+            state.isLoading = false;
+        },
+    },
 });
 
-export const { toggleGptSearch } = gptSlice.actions;
+export const {
+    toggleGptSearch,
+    setGptLoading,
+    addGptMovieResults,
+} = gptSlice.actions;
+
 export default gptSlice.reducer;
