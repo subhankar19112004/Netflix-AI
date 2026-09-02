2e1f2fbc82985b8a09ba35e2442d53a35919146c
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-09-02 10:16:31 +0530

    feat: update GptSearch component layout to include MovieSuggestions and enhance styling

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,9 +1,11 @@
 import GptSearchBar from "./GPT/GptSearchBar"
+import MovieSuggestions from "./GPT/MovieSuggestions"
 
 const GptSearch = () => {
   return (
-    <div className="bg-[#162e31] h-screen">
+    <div className="bg-[#162e31] min-h-screen flex flex-col items-center justify-center gap-4 py-6 px-4">
       <GptSearchBar />
+      <MovieSuggestions/>
     </div>
   )
 }
